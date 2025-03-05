e1acb8c2a68d5a8af4b59e2f688c38c5cfdb3eb1
SeeYa <89501533+Lixeer@users.noreply.github.com>
2025-03-05 11:59:34 +0800

    Add files via upload

---
+++ ModEntry.cs
@@ -0,0 +1,54 @@
+﻿
+using HarmonyLib;
+using Lidgren.Network;
+using System;
+
+
+namespace StardewModdingAPI;
+
+
+
+public class ModEntry: Mod
+{   
+
+    
+    public override void Entry(IModHelper helper)
+
+    {   
+        
+        //int oldPort = this.Helper.Reflection.GetField<int>(typeof(LidgrenServer),"defaultPort").GetValue();
+        //Monitor.Log($"Old port is {oldPort}", LogLevel.Info);
+
+        var harmony = new Harmony("bilibili.Lixeer.changeserverport");
+         harmony.Patch(
+                original: AccessTools.Method(typeof(NetPeerConfiguration), "set_Port"), 
+                prefix: new HarmonyMethod(typeof(ModEntry), nameof(SetPortPrefix))     
+            );       
+
+    }
+    public static bool SetPortPrefix(ref int value)
+    {
+        string path = @"Mods/ChangeServerPort/config.txt";
+        try{
+            string text = File.ReadAllText(path);
+            Console.WriteLine("[ChangeServerPort]The new port is: " + text);
+            int port = int.Parse(text);
+            value = port;
+
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine("[ChangeServerPort]读取文件时出错: " + ex.Message);
+            value = 24643;
+        }
+
+        
+        
+            
+
+            
+        return true; // 继续执行原始方法
+    }
+
+}
